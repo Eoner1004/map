800fc626a3ac163f3b782404de1390ceb5bca6ae
hy <347920978@qq.com>
2021-07-29 16:13:49 +0800

    注释代码删除

--- example/Map.jsx
+++ example/Map.jsx
@@ -2,29 +2,26 @@ import React from 'react'
 
 import { CommonMap, MultiwindowMap, RollerMap, TimeSeriesMap } from '../src'
 
-// 感觉整个Map向是一个测试用的，如果粒度细的话，Map应该不在这个组件里出现，而是在业务系统，你考虑下，在优化下
-// 可以放在example目录中进行测试，这些都是不提交到仓库中的
 export default class Map extends React.Component {
 
     render() {
         const { url, isMapType, serviceType, serviceUrl } = this.props
 
         return <div>
-            {isMapType === 'normal' && <CommonMap
-                serviceType={serviceType}
-                serviceUrl={serviceUrl}
-                url={url}
-            >
-            </CommonMap>}
+            {isMapType === 'normal' &&
+                <CommonMap
+                    serviceType={serviceType}
+                    serviceUrl={serviceUrl}
+                    url={url}
+                >
+                </CommonMap>}
 
-            {
-                isMapType === 'timeSeries' && <TimeSeriesMap
+            {isMapType === 'timeSeries' &&
+                <TimeSeriesMap
                     url={url}
                     serviceType={serviceType}
                     serviceUrl={serviceUrl}
-
-                ></TimeSeriesMap>
-            }
+                ></TimeSeriesMap>}
 
             {isMapType === 'multiwindow' &&
                 <MultiwindowMap
@@ -38,8 +35,7 @@ export default class Map extends React.Component {
                     url={url}
                     serviceType={serviceType}
                     serviceUrl={serviceUrl}
-                ></RollerMap>
-            }
+                ></RollerMap>}
         </div>
     }
 }
